497ef93cb85b41fc535a466fc6ed5ba78288eeb4
qStivi <stephanglaue@gmail.com>
2022-08-31 18:21:12 +0200

    disable repeat on skip
    Signed-off-by: qStivi <stephanglaue@gmail.com>

--- src/main/java/de/qStivi/commands/SkipCommand.java
+++ src/main/java/de/qStivi/commands/SkipCommand.java
@@ -21,6 +21,7 @@ public class SkipCommand implements ICommand {
     @Override
     public void handle(SlashCommandInteractionEvent event) {
         PlayerManager.getINSTANCE().skip(event.getGuild());
+        PlayerManager.getINSTANCE().setRepeat(event.getGuild(), false);
         event.getHook().editOriginal("Skipping....").delay(Duration.ofSeconds(10)).flatMap(Message::delete).queue();
     }
 
