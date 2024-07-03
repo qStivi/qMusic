3288c03f4343c8424847b467ce8f298433a69984
qStivi <stephanglaue@googlemail.com>
2024-07-03 20:59:07 +0200

    Stuff
    Signed-off-by: qStivi <stephanglaue@googlemail.com>

--- src/main/java/de/qStivi/Main.java
+++ src/main/java/de/qStivi/Main.java
@@ -9,14 +9,11 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 // TODO what happens when the bot is added to a guild while its running?
 public class Main {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static JDA JDA;
 
     public static void main(String[] args) {
